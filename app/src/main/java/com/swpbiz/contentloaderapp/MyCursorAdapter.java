ecc225d487fb0696ec2a6408be8f2c5f47e1c19f
Raymond Lee <rayleeriver@gmail.com>
2015-03-21 12:09:38 -0700

    wip:  recyclerview

---
+++ app/src/main/java/com/swpbiz/contentloaderapp/MyCursorAdapter.java
@@ -0,0 +1,27 @@
+package com.swpbiz.contentloaderapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+public class MyCursorAdapter extends CursorAdapter {
+
+
+    public MyCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.item_contact, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+//        TextView tv
+    }
+}
